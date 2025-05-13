9ac8d88b784bdb46defc768a8f98b991e27c90b4
AlbericoJr <40373171+AlbericoJr@users.noreply.github.com>
2025-05-13 16:52:44 -0300

    Criação de app de academia UpFit

---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+import { Center, Spinner } from "@gluestack-ui/themed"
+
+export function Loading() {
+  return (
+    <Center flex={1} bg="$gray700">
+      <Spinner
+        color="warning500"
+        size="large"
+        accessibilityLabel="Carregando"
+      />
+    </Center>
+  )
+}
